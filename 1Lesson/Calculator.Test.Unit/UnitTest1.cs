fd6b94636d152c666794a968a3d19d592a622cf4
ChristopherDolan <202305666@post.au.dk>
2025-02-03 16:13:52 +0100

    Added test project

---
+++ 1Lesson/Calculator.Test.Unit/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace Calculator.Test.Unit;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
