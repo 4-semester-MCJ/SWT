05e8a80212b7e93b8b1ff4f2671d4afcdefb114e
ChristopherDolan <202305666@post.au.dk>
2025-02-03 14:33:33 +0100

    1Lesson

--- 1Lesson/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
+++ 1Lesson/Calculator/obj/Debug/net8.0/Calculator.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03a664f9aa7522d737ad258d325549a21c17f209")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
